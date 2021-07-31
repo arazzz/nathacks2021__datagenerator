aeb2ad07f66fcce1047c02cf5445af6cdb28ba47
dechengwang <35814297+dechengwang@users.noreply.github.com>
2021-07-31 15:24:16 -0600

    added ways to calculate TBR

--- src/python/datagen.py
+++ src/python/datagen.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sliders_namespace as sn
+#import sliders_namespace as sn
 
 #to import (for each frequency)
 #   length of epoch
@@ -28,9 +28,9 @@ def generateNoisyWave(times, freq, amp, noise):
 
 def generate_data():
 
-    times = np.linspace(0,sn.epochDuration,sn.samplingRate) # One second at 250Hz
-    theta = generateNoisyWave(times, sn.tFreq, sn.tAmp, sn.tNoise) # (time, Freq, Amp, Noise)
-    beta = generateNoisyWave(times, sn.bFreq, sn.bAmp, sn.bNoise)
+    times = np.linspace(0,3,512) # One second at 250Hz
+    theta = generateNoisyWave(times, 5, 10, 2) # (time, Freq, Amp, Noise)
+    beta = generateNoisyWave(times, 15, 5, 2)
 
     xy_list = []
 
@@ -43,13 +43,13 @@ def generate_data():
     # plt.show()
 
 def fft(series):
-    fftData = np.fft.rfft(series)
-    freq = np.fft.fftfreq(len(series))*sn.samplingRate
+    fftData = np.fft.fft(series)/len(series)
+    freq = np.fft.fftfreq(len(series))*512
 
     freq = freq[0:50]
     fftData = fftData[0:50]
 
-    plt.plot(freq, fftData)
+    plt.plot(freq, fftData.imag)
     plt.xlabel("Frequency")
     plt.ylabel("Magnitude")
     plt.show()
@@ -57,17 +57,34 @@ def fft(series):
 def freqSpec(data):
     n = len(data)
     Y = np.fft.fft(data)/n # fft computing and normalization
-    Y = Y[1:int(n/2)]
+    Y = Y[:100 * 3]
     k = np.arange(n)
-    T = n*(1/sn.samplingRate)
+    T = n*(3/512)
     frq = k/T # two sides frequency range
-    frq = frq[1:int(n/2)] # one side frequency range
-    pw = np.abs(Y)
-    return frq, pw
+    frq = frq[:100 * 3] # one side frequency range
+    amp = np.abs(Y)
+    xy_list = []
+    for x in range(0, len(frq)):
+        xy_list.append([frq[x], amp[x]])
+
+    return xy_list
+
+def power_spec(data):
+    ffsData = freqSpec(data)
+    for x in ffsData:
+        x[1] = x[1]**2
+
+    return ffsData
 
 def plotFS(data):
-    frq, pw = freqSpec(data)
+
+    ps = power_spec(data)
+
+    frq = [x[0] for x in ps]
+    pw = [x[1] for x in ps]
+
     plt.plot(frq, pw)
     plt.show()
 
+#fft(generate_data())
 plotFS(generate_data())
--- src/python/script.py
+++ src/python/script.py
@@ -9,9 +9,7 @@ import sliders_namespace as sn
 #   noise
 
 # returns
-epoch = []
-spectrum = []
-bins = []
+curr_data = []
 
 def generateNoisyWave(times, freq, amp, noise):
     
@@ -27,6 +25,8 @@ def generateNoisyWave(times, freq, amp, noise):
 
 def generate_data(args = {}):
 
+    global curr_data
+
     times = np.linspace(0,args.epochDuration,args.samplingRate) # One second at 250Hz
     theta = generateNoisyWave(times, args.tFreq, args.tAmp, args.tNoise) # (time, Freq, Amp, Noise)
     beta = generateNoisyWave(times, args.bFreq, args.bAmp, args.bNoise)
@@ -36,5 +36,58 @@ def generate_data(args = {}):
     for x in range(0, len(times)):
         xy_list.append([times[x], theta[x] + beta[x]])
 
+    curr_data = xy_list
+
     # return beta + theta
     return xy_list
+
+def ffs(data, samp_rate, epoch_length):
+
+    volts = [x[1] for x in data]
+
+    n = len(volts)
+    Y = np.fft.fft(volts)/n # fft computing and normalization
+    Y = Y[:100 * epoch_length]
+    k = np.arange(n)
+    T = n*(epoch_length/samp_rate)
+    frq = k/T # two sides frequency range
+    frq = frq[:100 * epoch_length] # one side frequency range
+    amp = np.abs(Y)
+
+    xy_list = []
+    for x in range(0, len(frq)):
+        xy_list.append([frq[x], amp[x]])
+
+    return xy_list
+
+def power_spec(data, samp_rate, epoch_length):
+    ffsData = ffs(data, samp_rate, epoch_length)
+    for x in ffsData:
+        x[1] = x[1]**2
+
+    return ffsData
+
+def get_TBR_power(args = {}):
+
+    global curr_data
+
+    ffsData = power_spec(curr_data, args.samplingRate, args.epochDuration)
+
+    theta_sum = 0
+    theta_count = 0
+    beta_sum = 0
+    beta_count = 0
+
+    for x in ffsData:
+        if x[0] <=7 and x[0] > 3.5:
+            theta_sum += x[1]
+            theta_count += 1
+        elif x[0] <= 20 and x[0] > 12:
+            beta_sum += x[1]
+            beta_count += 1
+    
+    return ( (theta_sum / theta_count) / (beta_sum / beta_count) )
+
+    
+
+    
